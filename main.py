3080f26e965d02c98b0ef959086ca350a5985c80
mariatorreschuco-hue <mariatorreschuco@gmail.com>
2026-08-12 08:45:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def saludar():
+    print("----------------------------------------")
+    print("¡Hola desde el pipeline de GitHub Actions!")
+    print(f"Versión de Python ejecutándose: {sys.version}")
+    print("----------------------------------------")
+
+if __name__ == "__main__":
+    saludar()
